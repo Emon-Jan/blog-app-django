3364b8ef64088dadf3336fe99a5c2f98aa2767ed
Muktadir Imam Jan <muktadirimam@gmail.com>
2018-06-20 03:49:53 +0600

    main urls login and logout routing added

--- blog_webapp/urls.py
+++ blog_webapp/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_view
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
+    path('', include('blogapp.urls')),
+    path('accounts/login/', auth_view.login, name='login'),
+    path('accounts/logout/', auth_view.logout,
+         name='logout', kwargs={'next_page': '/'}),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
